7df0be1de513115f54d3190b0ee82cd705d829be
Jad Samara <jadsamara@yahoo.com>
2024-05-29 23:25:08 +0300

    force

--- numble/numble/settings.py
+++ numble/numble/settings.py
@@ -17,9 +17,6 @@ import oracledb
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
