20fb8123bc7d0b8e844d78bbe51adb5a22bd0e00
Jad Samara <jadsamara@yahoo.com>
2024-10-24 22:47:14 +0300

    Fix import issues for Vercel deployment

--- numble/manage.py
+++ numble/manage.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+from django.core.management import execute_from_command_line
+from django.core.wsgi import get_wsgi_application
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "numble.settings")
-    try:
-        from django.core.management import execute_from_command_line
-        from django.core.wsgi import get_wsgi_application
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    
-    # Create the WSGI application callable.
-    application = get_wsgi_application()
+# Set the default settings module for the 'manage.py' command.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')  # Replace 'your_project_name' with your actual project name.
 
+# Create the WSGI application callable.
+application = get_wsgi_application()
+
+if __name__ == '__main__':
     execute_from_command_line(sys.argv)
-
-
-if __name__ == "__main__":
-    main()
