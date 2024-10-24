7e330a0700350c3810d3fabed84e4a231aed4b82
Jad Samara <jadsamara@yahoo.com>
2024-10-24 22:32:48 +0300

    vercel settings

--- numble/manage.py
+++ numble/manage.py
@@ -9,12 +9,17 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "numble.settings")
     try:
         from django.core.management import execute_from_command_line
+        from django.core.wsgi import get_wsgi_application
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    # Create the WSGI application callable.
+    application = get_wsgi_application()
+
     execute_from_command_line(sys.argv)
 
 
